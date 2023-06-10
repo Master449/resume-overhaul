5acb0b2edf0d41062f3f8944875df4ab6c68ff9c
David Flowers <master4491@gmail.com>
2023-06-09 22:05:47 -0500

    Need to fix the sidebar, needs to stick

--- src/App.vue
+++ src/App.vue
@@ -4,136 +4,58 @@ import { RouterLink, RouterView } from 'vue-router'
 
 <template>
   <div class="flex-container">
-  <div class="flexitems">
-  <div id="mobile" v-if="isMobile">
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="#">
-          <img src="/img/avatar.png" alt="" width="40" />
-          David Flowers II</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+    <div class="flexitems">
+      <div id="Nav-Not-Mobile">
+        <div id="navbar" class="d-flex flex-column flex-shrink-0 p-3 text-white bg-dark">
+          <img src="/img/avatar.png" />
+          <hr>
+          <h3 class="text-center"><strong>David Flowers II</strong></h3>
+          <p class="text-center m-1"><i>Computer Science Undergrad</i></p>
+          <p class="text-center m-2"><i>dsflowers2000@gmail.com</i></p>
+          <hr>
+          <ul class="nav nav-pills flex-column mb-auto">
             <li class="nav-item">
-              <a href="#" class="nav-link active" aria-current="page">
-                <i class="fa-solid fa-house"></i> Home
-              </a>
+              <router-link to="/" class="nav-link"><i class="fa-solid fa-house"></i> Home</router-link>
             </li>
-            <li class="nav-item">
-              <a href="#" class="nav-link text-white">
-                <i class="fa-solid fa-briefcase"></i>
-                Experience
-              </a>
+            <li>
+              <router-link to="/experience" class="nav-link"><i class="fa-solid fa-briefcase"></i> Experience</router-link>
             </li>
-            <li class="nav-item">
-              <a href="#" class="nav-link text-white">
-                <i class="fa-solid fa-school"></i>
-                Education
-              </a>
+            <li>
+              <router-link to="/education" class="nav-link"><i class="fa-solid fa-school"></i> Education</router-link>
             </li>
-            <li class="nav-item">
-              <a href="#" class="nav-link text-white">
-                <i class="fa-brands fa-github"></i>
-                Projects
-              </a>
+            <li>
+              <router-link to="/projects" class="nav-link"><i class="fa-brands fa-github"></i> Projects</router-link>
             </li>
+          </ul>
+          <hr>
+          <ul class="links list-group list-group-flush list-group-horizontal-md">
+            <li class="list-group-item"><a href="https://github.com/Master449" class="stretched-link"><i
+                  class="fa-brands fa-github"></i></a></li>
+            <li class="list-group-item"><a href="https://www.linkedin.com/in/dflowersii" class="stretched-link"><i
+                  class="fa-brands fa-linkedin"></i></a></li>
+            <li class="list-group-item"><a href="https://github.com/Master449" class="stretched-link"><i
+                  class="fa-brands fa-twitter"></i></a></li>
           </ul>
         </div>
       </div>
-    </nav>
-  </div>
-  <div id="Nav-Not-Mobile" v-else>
-    <div id="navbar" class="d-flex flex-column flex-shrink-0 p-3 text-white bg-dark">
-      <img src="/img/avatar.png" />
-      <hr>
-      <h3 class="text-center"><strong>David Flowers II</strong></h3>
-      <p class="text-center m-1"><i>Computer Science Undergrad</i></p>
-      <p class="text-center m-2"><i>dsflowers2000@gmail.com</i></p>
-      <hr>
-      <ul class="nav nav-pills flex-column mb-auto">
-        <li class="nav-item">
-          <router-link to="/" class="nav-link"><i class="fa-solid fa-house"></i> Home</router-link>
-        </li>
-        <li>
-          <router-link to="/experience" class="nav-link"><i class="fa-solid fa-briefcase"></i> Experience</router-link>
-        </li>
-        <li>
-          <router-link to="/education" class="nav-link"><i class="fa-solid fa-school"></i> Education</router-link>
-        </li>
-        <li>
-          <router-link to="/projects" class="nav-link"><i class="fa-brands fa-github"></i> Projects</router-link>
-        </li>
-      </ul>
-      <hr>
-      <ul class="links list-group list-group-flush list-group-horizontal-md">
-        <li class="list-group-item"><a href="https://github.com/Master449" class="stretched-link"><i
-              class="fa-brands fa-github"></i></a></li>
-        <li class="list-group-item"><a href="https://www.linkedin.com/in/dflowersii" class="stretched-link"><i
-              class="fa-brands fa-linkedin"></i></a></li>
-        <li class="list-group-item"><a href="https://github.com/Master449" class="stretched-link"><i
-              class="fa-brands fa-twitter"></i></a></li>
-      </ul>
+    </div>
+    <div class="flexitems">
+        <RouterView />
     </div>
   </div>
-</div>
-<div class="flexitems">
-  <RouterView />
-</div>
-</div>
 </template>
 
 <style>
-html {
-  height: 100vh;
-}
-
-* {
-  box-sizing: border-box;
-}
-
-body {
-  background-color: #323b44;
-  color: white;
-  margin: 0;
-}
-
-.card {
-  background-color: #212529;
-  color: white;
-}
-
-.flex-container {
-  display: flex;
-  height: 100vh;
-  flex-direction: row;
-}
-
-.flex-items:nth-child(1) {
-  flex: 1;
-  flex-grow: 0;
-  flex-shrink: 1;
-}
-
-.flex-items:nth-child(2) {
-  display: block;
-  flex-grow: 1;
-  flex-shrink: 1;
-  flex-basis: auto;
-  margin: 5%;
-}
-
 #navbar {
   height: 100%;
   width: 100%;
+  position: -webkit-sticky;
+  position: sticky;
 }
 
 #Nav-Not-Mobile {
   margin: 0;
   padding: 0;
-  min-height: 100vh;
   height: 100vh;
 }
 
@@ -150,18 +72,5 @@ body {
   width: 100%;
   height: 100%;
   font-size: 20px;
-}
-
-.mobileImg {
-  height: 5%;
-  margin: 0;
-}
-
-#mobile {
-  margin: 0;
-  padding: 0;
-  z-index: 2;
-  position: absolute;
-  width: 100%;
 }
 </style>
--- src/components/Education.vue
+++ src/components/Education.vue
@@ -29,6 +29,30 @@ export default {
                     start: "August 2016",
                     end: "May 2018",
                     info: "Pursuing a Bachelor of Science in Computer Science, with a focus on Software. Developing hands-on experience in software development, data structures and algorithms, databases, and computer architecture. Building a strong foundation in computational thinking and problem-solving skills, and preparing to apply technical knowledge to real-world challenges and create innovative solutions upon graduation."
+                },
+                {
+                    logo: "/img/KEC.png",
+                    name: "Kishwaukee Education Consortium",
+                    study: "Technical Training",
+                    start: "August 2016",
+                    end: "May 2018",
+                    info: "Pursuing a Bachelor of Science in Computer Science, with a focus on Software. Developing hands-on experience in software development, data structures and algorithms, databases, and computer architecture. Building a strong foundation in computational thinking and problem-solving skills, and preparing to apply technical knowledge to real-world challenges and create innovative solutions upon graduation."
+                },
+                {
+                    logo: "/img/KEC.png",
+                    name: "Kishwaukee Education Consortium",
+                    study: "Technical Training",
+                    start: "August 2016",
+                    end: "May 2018",
+                    info: "Pursuing a Bachelor of Science in Computer Science, with a focus on Software. Developing hands-on experience in software development, data structures and algorithms, databases, and computer architecture. Building a strong foundation in computational thinking and problem-solving skills, and preparing to apply technical knowledge to real-world challenges and create innovative solutions upon graduation."
+                },
+                {
+                    logo: "/img/KEC.png",
+                    name: "Kishwaukee Education Consortium",
+                    study: "Technical Training",
+                    start: "August 2016",
+                    end: "May 2018",
+                    info: "Pursuing a Bachelor of Science in Computer Science, with a focus on Software. Developing hands-on experience in software development, data structures and algorithms, databases, and computer architecture. Building a strong foundation in computational thinking and problem-solving skills, and preparing to apply technical knowledge to real-world challenges and create innovative solutions upon graduation."
                 }
             ]
         }
@@ -47,43 +71,24 @@ export default {
 </script>
 
 <template>
-    <div id="Non-Mobile" v-if="!isMobile">
-        <h1>Education</h1>
-        <hr />
-        <div class="card mb-4" v-for="(school, index) in Education" :key="index">
-            <div class="row g-0">
-                <div class="col-md-2">
-                    <img :src="school.logo" class="img-fluid rounded-start" :alt="school.name">
-                </div>
-                <div class="col">
-                    <div class="card-body">
-                        <h5 class="card-title"><strong>{{ school.name }} - {{ school.study }}</strong></h5>
-                        <p class="card-text">{{ school.start }} - {{ school.end }}</p>
-                        <p class="card-text">{{ school.info }}</p>
-                    </div>
+<div id="contain">
+    <h1>Education</h1>
+    <hr />
+    <div class="card mb-4" v-for="(school, index) in Education" :key="index">
+        <div class="row g-0">
+            <div class="col-md-2">
+                <img :src="school.logo" class="img-fluid rounded-start" :alt="school.name">
+            </div>
+            <div class="col">
+                <div class="card-body">
+                    <h5 class="card-title"><strong>{{ school.name }} - {{ school.study }}</strong></h5>
+                    <p class="card-text">{{ school.start }} - {{ school.end }}</p>
+                    <p class="card-text">{{ school.info }}</p>
                 </div>
             </div>
         </div>
     </div>
-    <div id="Mobile" v-else>
-        <h1>Education</h1>
-        <hr />
-        <div class="card mb-4" v-for="(school, index) in Education" :key="index">
-            <div class="row g-2">
-                <div class="col-md-2">
-                    <img :src="school.logo" class="img-fluid rounded-start" :alt="school.name">
-                </div>
-                <div class="col">
-                    <div class="card-body">
-                        <h5 class="card-title"><strong>{{ school.name }}</strong></h5>
-                        <h4 class="card-text">{{ school.study }}</h4>
-                        <p class="card-text">{{ school.start }} - {{ school.end }}</p>
-                        <p class="card-text">{{ school.info }}</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+</div>
 </template>
 
 <style scoped>
@@ -92,7 +97,7 @@ hr {
     border: 2px solid white;
 }
 
-#Mobile {
-    margin-top: 20%;
+#contain {
+    margin: 5%;
 }
 </style>
