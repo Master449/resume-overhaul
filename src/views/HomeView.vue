d6e0595b0861ac7c0da0b2ed2820e4aef0689ddd
David Flowers <master4491@gmail.com>
2023-06-11 22:47:20 -0500

    Wrote down some ideas before I forget about them.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,10 @@
 <script setup lang="ts">
 import TheWelcome from '../components/index-compo.vue'
+
+/**
+ * Some Ideas I had late at night:
+ *   - Skills section, like my current site, that can be moved with arrows (important ones first)
+ */
 </script>
 
 <template>
