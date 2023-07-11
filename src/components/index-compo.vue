4578e3ea27b283b559f9e18f2486fc00a3efec68
David Flowers II <master4491@gmail.com>
2023-07-10 21:08:58 -0500

    Added a lot more information to index page. Unsure if I like the design descisions

--- src/components/index-compo.vue
+++ src/components/index-compo.vue
@@ -12,35 +12,63 @@ export default {
 
 <template>
     <div class="container">
-        <p class="display-3">David Flowers II</p>
-    <p class="display-6">Full-Stack Developer</p>
-    <hr />
-    <div class="parent">
-        <div>
-            <i class="fa-solid fa-code-branch"></i>
-            <p class="skills">C++, Python, PHP, JS</p>
+        <p class="display-2">David Flowers II</p>
+        <p class="display-6">Full-Stack Developer and Computer Technician</p>
+        <hr />
+        <div class="content-container">
+            <h1>About Myself</h1>
+            <p class="content">
+                Fueled by a lifelong love of computers, I have taken every opportunity to further my
+                knowledge and skills in the
+                field. Starting with dual-credit courses at my local community college during high school, I went on to earn
+                an
+                Associate of Science degree from Kishwaukee College. Currently, I am pursuing a Bachelor of Computer Science
+                at
+                Northern Illinois University, where I am deepening my understanding of computer systems, algorithms, and
+                programming languages. With each new step, I am solidifying my passion for technology and preparing to bring
+                my
+                expertise to real-world challenges and impactful solutions.</p>
         </div>
-        <div>
-            <i class="fa-solid fa-database"></i>
-            <p class="skills">SQL, MariaDB, SQLite3</p>
+        <h1>Skillsets</h1>
+        <p class="content">Whether skills on the job, or in the classroom, they are listed here proudly.</p>
+        <div class="major-container">
+            <div class="parent-major">
+                <div>
+                    <i class="fa-solid fa-toolbox"></i>
+                    <p class="skills">12+ Years Computer Repair</p>
+                </div>
+                <div>
+                    <i class="fa-solid fa-user"></i>
+                    <p class="skills">7+ Years Customer Service</p>
+                </div>
+            </div>
         </div>
-        <div>
-            <i class="fa-solid fa-terminal"></i>
-            <p class="skills">Linux, PowerShell, Git</p>
+        <div class="parent">
+            <div>
+                <i class="fa-solid fa-code-branch"></i>
+                <p class="skills">C++, PHP, JS, git</p>
+            </div>
+            <div>
+                <i class="fa-solid fa-database"></i>
+                <p class="skills">SQL and DBMS</p>
+            </div>
+            <div>
+                <i class="fa-solid fa-terminal"></i>
+                <p class="skills">Bash and PowerShell</p>
+            </div>
+            <div>
+                <i class="fa-solid fa-memory"></i>
+                <p class="skills">Computer Repair</p>
+            </div>
+            <div>
+                <i class="fa-solid fa-server"></i>
+                <p class="skills">Linux Servers</p>
+            </div>
+            <div>
+                <i class="fa-solid fa-object-group"></i>
+                <p class="skills">Adobe Suite</p>
+            </div>
         </div>
-        <div>
-            <i class="fa-solid fa-desktop"></i>
-            <p class="skills">Computer Repair</p>
-        </div>
-        <div>
-            <i class="fa-solid fa-pen-nib"></i>
-            <p class="skills">Adobe and Microsoft Office</p>
-        </div>
-        <div>
-            <i class="fa-solid fa-user"></i>
-            <p class="skills">7+ Years Customer Service</p>
-        </div>
-    </div>
     </div>
 </template>
 
@@ -52,13 +80,58 @@ hr {
 }
 
 i {
+    vertical-align: middle;
+    align-content: center;
+}
+
+h2,
+h3,
+h4,
+h5 {
+    text-align: center;
+}
+
+h1 {
+    margin-top: 7%;
+}
+
+.content {
+    font-size: 15pt;
+    text-align: justify;
+}
+
+.container {
+    display: block;
+    width: 80%;
+}
+
+.parent i {
     font-size: 2.25em;
+    text-align: center;
+    height: 2.25em;
+    width: 2.25em;
     color: white;
     float: left;
     padding: 5%;
     margin: 0 5% 0 0;
     vertical-align: middle;
     background-color: #212529;
+    border-radius: 25px;
+}
+
+.parent-major i {
+    font-size: 2.5em;
+    text-align: center;
+    vertical-align: middle;
+    align-content: center;
+    height: 2.25em;
+    width: 2.25em;
+    color: black;
+    float: left;
+    padding: 5%;
+    margin: 0 5% 0 0;
+    vertical-align: middle;
+    background: linear-gradient(to bottom, #bcc6cc, #eee, #bcc6cc);
     border-radius: 25px;
 }
 
@@ -71,11 +144,28 @@ i {
 .parent {
     display: grid;
     grid-template-columns: repeat(3, 1fr);
-    grid-template-rows: repeat(3, 1fr);
+    grid-template-rows: repeat(2, 1fr);
     grid-column-gap: 10px;
     grid-row-gap: 10px;
     margin: 2% 0;
     width: 100%;
+}
+
+.major-container {
+    display: block;
+    margin: 0 auto;
+    width: 90%;
+}
+
+.parent-major {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    grid-column-gap: 100px;
+    grid-row-gap: 10px;
+    margin: 2% 0;
+    width: 100%;
+    align-items: center;
+    justify-content: center;
 }
 
 .display-1,
