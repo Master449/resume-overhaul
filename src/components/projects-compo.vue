dd4ff8cb2070eeb304203bc57fa6ed8465d263c4
David Flowers II <master4491@gmail.com>
2023-07-13 19:41:19 -0500

    All projects are now hooked up to githubs API to get the readmes.

--- src/components/projects-compo.vue
+++ src/components/projects-compo.vue
@@ -13,7 +13,7 @@ export default {
                 {
                     picture: "https://picsum.photos/200/100/?blur=8",
                     name: "Rimjobs Automotive Store",
-                    info: "A Group Project for CSCI 467: Software Engineering. For this project we we're given a purpose; Ours being an Online Auto Parts Store. This project had a few constraints, like a legacy database that was read only, to a remote Credit Card Authorization System. Other than that we had total freedom. My responsibilities included: MySQL for our Database, Managing Linux users and groups on Linode, and API Endpoints for the product catalogue, and shopping cart. The name was a joke that stuck. The name came from the Saints Row franchise.",
+                    info: "",
                     stack: ["Vue", "Express js", "Bootstrap", "SCSS", "SQLite3", "Linode"],
                     url: "https://github.com/467Group3A/rimjobs"
                 },
@@ -56,11 +56,15 @@ export default {
         fetch("https://raw.githubusercontent.com/master449/resume-overhaul/dev/README.md")
         .then(response => response.text())
         .then(data => (this.Projects[0].info += data));
+        fetch("https://raw.githubusercontent.com/467Group3A/rimjobs/master/README.md")
+        .then(response => response.text())
+        .then(data => (this.Projects[1].info += data.slice(1,297) + " My responsibilities included: MySQL for our Database, Managing Linux users and groups on Linode, and API Endpoints for the product catalogue, and shopping cart. The name was a joke that stuck. The name came from the Saints Row franchise."));
         fetch("https://raw.githubusercontent.com/master449/opossum-game/main/README.md")
         .then(response => response.text())
         .then(data => (this.Projects[2].info += data));
     }
 }
+// My responsibilities included: MySQL for our Database, Managing Linux users and groups on Linode, and API Endpoints for the product catalogue, and shopping cart. The name was a joke that stuck. The name came from the Saints Row franchise.
 </script>
 
 <template>
